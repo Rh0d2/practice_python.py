9e65a579bf93dcad0827bcf8660a3c4cc01fb6b1
Rod <rodjohnguillen@gmail.com>
2026-03-09 23:17:05 +0800

    first practice done.

---
+++ practice_python.py
@@ -0,0 +1,13 @@
+#collect number
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+#print smaller number
+if num1 > num2: 
+    print(num2, "is smaller than", num1)
+
+elif num1 < num2:
+        print(num1, "is smaller than", num2)   
+        
+else:
+    print("Both numbers are equal.")
