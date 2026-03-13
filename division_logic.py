f72232af4e11625e7d3bbc8a92fb685bf979bf24
Rod <rodjohnguillen@gmail.com>
2026-03-13 23:03:17 +0800

    difference logic

--- division_logic.py
+++ division_logic.py
@@ -2,6 +2,6 @@
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
-#division logic
+$
 print(num1/num2)
 
---
+++ print_the_difference.py
@@ -0,0 +1,8 @@
+#Collect numbers
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+#Difference logic
+result = (num1 - num2)
+print(result)
+
