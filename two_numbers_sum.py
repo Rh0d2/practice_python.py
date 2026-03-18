9ced1164b7cbb72dd96896f4c681ea109a23b155
Rod <rodjohnguillen@gmail.com>
2026-03-18 21:46:26 +0800

    sum of two numbers logic

---
+++ two_numbers_sum.py
@@ -0,0 +1,7 @@
+#Collect numbers
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+#No decimal devision logic
+result = num1 + num2
+print("The sum of the two numbers is", result)
