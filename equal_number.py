15841be72fe2b2ccfb901a9d583c945bab12cf68
Rod <rodjohnguillen@gmail.com>
2026-03-18 21:38:33 +0800

    equal number logic

---
+++ equal_number.py
@@ -0,0 +1,9 @@
+#Collect numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+#equal numbers logic
+if num1 == num2:
+ print("Equal")
+else:
+ print("Error")
