9c17135e170e52b30b8bb40e9e4bdfd9dede29da
Rod <rodjohnguillen@gmail.com>
2026-03-19 21:41:51 +0800

    Add logic that remove duplicate

---
+++ practice_python3/display_first_duplicate.py
@@ -0,0 +1,12 @@
+#start
+numbers = []
+result = 0
+
+#collect numbers
+for i in range(10):
+    val = int(input(f"Enter number {i + 1}: "))
+    #Turn collected values into list
+    numbers.append(val)
+    #Remove duplicates
+    result = list(dict.fromkeys(numbers))
+    print(result)
