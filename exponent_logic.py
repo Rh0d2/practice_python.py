f1d1704b77863a6ca3fffa639b3377bdde2181a4
Rod <rodjohnguillen@gmail.com>
2026-03-19 09:16:18 +0800

    Exponen logic

---
+++ exponent_logic.py
@@ -0,0 +1,7 @@
+#Collect numbers
+num1 = int(input("Enter your first number: "))
+num2 = int(input("Enter your second number: "))
+
+#Exponent logic
+result = num1 ** num2
+print("The result when the first number is raised to the second number is", result)
