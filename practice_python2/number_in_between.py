76f8d0e2f3c0af84e41cc4796c0cfb890b6731a8
Rod <rodjohnguillen@gmail.com>
2026-03-19 21:26:29 +0800

    fixed range logic

--- practice_python2/number_in_between.py
+++ practice_python2/number_in_between.py
@@ -3,5 +3,6 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 #Logic
-while i in range(num1 + 1, num2):
+for i in range(num1 +1, num2):
     print (i)
+
