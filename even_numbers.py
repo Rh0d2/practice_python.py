c560c10cf7f683519e33468ae4f86f05a12c7831
Rod <rodjohnguillen@gmail.com>
2026-03-19 13:16:31 +0800

    Print even numbers

--- even_numbers.py
+++ even_numbers.py
@@ -2,10 +2,10 @@
 numbers = []
 
 #Collect numbers
-for i in range(10):
+for i in range(101):
     val = int(input(f"Enter number {i + 1}: "))
     #Even logic
     if val % 2 == 0:
         numbers.append(val)
 
-print(f"All the even numbers starting from 0 to 10 are {numbers}")
+print(f"All the even numbers starting from 0 to 101 are {numbers}")
