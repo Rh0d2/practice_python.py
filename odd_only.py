7bd772d2e0bc25b0ae5e9e5bf4b89ee27a7f7c13
Rod <rodjohnguillen@gmail.com>
2026-03-19 12:54:11 +0800

    Count odd numbers

--- odd_only.py
+++ odd_only.py
@@ -1,11 +1,12 @@
 #start 
 total_odd = 0
+numbers = []
 
 #collect numbers
-
 for i in range(10):
     val = int(input(f"Enter number {i + 1}: "))
+    #Logic to determine odd numbers
     if val % 2 != 0:
-        total_odd = val
+        total_odd += 1
 
 print(f"There are {total_odd} odd numbers")
