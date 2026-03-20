bebe4d39dc71235f3cff38c8125fb46cc3bed138
Rod <rodjohnguillen@gmail.com>
2026-03-20 09:00:48 +0800

    The lowest number will display once Value is Error.

--- practice_python3/display_lowest.py
+++ practice_python3/display_lowest.py
@@ -1,12 +1,12 @@
-#Start
 history = []
 
+
 while True:
-    try: 
+    try:
         val = int(input("Enter the number: "))
         history.append(val)
 
     except ValueError:
         if history:
-            print(f"Error! The lowest number is {min(history)}")
+            print(f"Error! The lowest number is: {min(history)}")
         break
