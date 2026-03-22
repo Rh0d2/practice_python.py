74b7f436ed5326ca1c51a2e88dabd18fcda8a8bf
Rod <rodjohnguillen@gmail.com>
2026-03-22 23:19:42 +0800

    Use sorted() function insted of sort()

--- practice_python3/sort_function.py
+++ practice_python3/sort_function.py
@@ -1,13 +1,16 @@
-#Start
+# Start
 history = []
 
-#Collect the input
+# Collect the infinitely untill the ValueError
 while True:
     try:
         value = int(input("Enter the number: "))
+        # Store the user input in the list
         history.append(value)
 
+    # Detect letters or symbols
     except ValueError:
+        # Display the entered value from lowest to highest
         if history:
-            print(f"Error. The entered numbers are: {sort(history)} ") 
+            print(f"Error. The entered numbers from lowest to highest are: {sorted(history)} ") 
         break
