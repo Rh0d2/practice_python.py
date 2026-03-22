e8113c1549cc553ba96a201389f432db8b055d1b
Rod <rodjohnguillen@gmail.com>
2026-03-23 00:14:36 +0800

    Fixed the logic error and add pseudocode

--- practice_python4/display_duplicated_num.py
+++ practice_python4/display_duplicated_num.py
@@ -5,7 +5,12 @@ number = 0
 # Collect inputs 10 times
 for i in range(10):
     user_input = int(input(f"Enrter the number{i +1}: "))
+    # Turn collected inputs into list
     history.append(user_input)
+
     number = history
-    duplicates = [num for num, freq in number.count() if  1]
-print(duplicates)
+
+    #Collect the duplicated numbers only
+    duplicates = list(set([num for num in history if history.count(num) > 1]))   
+
+print(f"The duplicated numbers are: {duplicates}")
