00e63abeb2448e66f8980201f6d6003e8e44e60f
Rod <rodjohnguillen@gmail.com>
2026-03-24 07:26:23 +0800

    Revised the logic of the program to print the most freq value.

--- practice_python4/most_duplicate.py
+++ practice_python4/most_duplicate.py
@@ -6,9 +6,13 @@ while True:
         history.append(user_input)
 
     except ValueError:
-        if history: 
-            most_duplicate = list(set([num for num in history if history.count(num) >= 2]))
+        if history:
+            most_duplicate = max(set(history), key = history.count)
+
+            if history.count(most_duplicate) > 1:
+                print(f"The most duplicated value is: {most_duplicate}")
+            else:
+                print("No duplicates were found.")
         
-        if most_duplicate:
-            print(f" The most duplicated value is: {max(most_duplicate)}")
+        # 3. Break must be inside 'except' but outside 'if history' to stop the loop
         break
