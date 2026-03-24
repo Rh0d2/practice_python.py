df628a7884b8bc58416bd6fc845ddf3c85138946
Rod <rodjohnguillen@gmail.com>
2026-03-24 08:40:43 +0800

    String manipulation using lstrip

--- practice_python5/remove_space.py
+++ practice_python5/remove_space.py
@@ -1,6 +1,10 @@
-print("Please input your name with several space characters in the beginning")
+# User instruction
+print("Please Enter your name with several space characters in the beginning")
+
+# Collect username
 user_name = str(input("Enter your name: "))
 
+# Remove space
 name = user_name.lstrip()
 
 print(f"Hi, {name}")
