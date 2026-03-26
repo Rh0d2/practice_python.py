4655140273f9230df013c4621c6e6fab6f88dcef
Rod <rodjohnguillen@gmail.com>
2026-03-27 03:28:45 +0800

    Add a manual program removeprefix()

---
+++ practice_python6/manual_removeprefix().py
@@ -0,0 +1,8 @@
+#This program will remove your set prefix
+user_text = (input("Enter text: "))
+prefix = (input("Enter the prefix to be removed: "))
+
+if user_text.startswith(prefix):
+    user_text = user_text[len(prefix):6]
+
+print(user_text)
